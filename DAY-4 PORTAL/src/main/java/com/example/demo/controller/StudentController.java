5290f5d55df1ff2e46ec92fe50d56b63096b27da
Srihari KR <99411231+Sriharikr06@users.noreply.github.com>
2023-02-24 14:44:08 +0530

    Update StudentController.java

--- DAY-4 PORTAL/src/main/java/com/example/demo/controller/StudentController.java
+++ DAY-4 PORTAL/src/main/java/com/example/demo/controller/StudentController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 
 import com.example.demo.model.Student;
 import com.example.demo.service.StudentService;
@@ -32,5 +36,14 @@ public class StudentController{
 	@GetMapping("/student/{id}")
 	public Optional<Student> readById(@PathVariable int id){
 		return service.getStudentById(id);
+	}
+	@PutMapping("/student")
+	public String update(@RequestBody Student student) {
+		return service.updateStudent(student);
+	}
+	
+	@DeleteMapping("/student")
+	public String delete(@RequestParam int id) {
+		return service.deleteStudentById(id);
 	}
 }
