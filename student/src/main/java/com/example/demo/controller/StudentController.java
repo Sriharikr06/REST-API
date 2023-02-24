cc9169ecf3691d1f2afccb1e241e5a6b5d26d39f
Srihari KR <hsri94393@gmail.com>
2023-02-24 12:12:44 +0530

    Industry Training-Web FrameWork

---
+++ student/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
+
+
+@RestController
+public class StudentController{
+	@Autowired
+	StudentService service;
+	
+	@PostMapping("/student")
+	public boolean add(@RequestBody Student student) {
+		return service.addStudent(student);
+	}
+	
+	@GetMapping("/student")
+	public List<Student> read(){
+		return service.getStudent();
+	}
+	
+	@GetMapping("/student/{id}")
+	public Optional<Student> readById(@PathVariable int id){
+		return service.getStudentById(id);
+	}
+}
