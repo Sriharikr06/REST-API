ac4f55630c739ce2ab5bd70ca4abf3169f3043ee
Srihari KR <99411231+Sriharikr06@users.noreply.github.com>
2023-02-26 21:47:52 +0530

    Update BabyController.java

--- DAY-6/baby/src/main/java/com/example/demo/controller/BabyController.java
+++ DAY-6/baby/src/main/java/com/example/demo/controller/BabyController.java
@@ -1,9 +1,47 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Baby;
+import com.example.demo.service.BabyService;
 
 @RestController
 public class BabyController {
+	@Autowired
+	BabyService service;
 	
-
+	@PostMapping("/Baby")
+	public boolean post(@RequestBody Baby baby) {
+		return service.addBaby(baby);
+	}
+	
+	@GetMapping("/Baby")
+	public List<Baby> get(){
+		return service.getAllBaby();
+	}
+	
+	@GetMapping("/Baby/{id}")
+	public Optional<Baby> getById(@PathVariable int id){
+		return service.getByBabyId(id);
+	}
+	
+	@GetMapping("/Baby/{offset}/{size}")
+	public List<Baby> getByPage(@PathVariable int offset ,@PathVariable int size){
+		return (service.getBabyByPagination(offset, size));
+	}
+	
+	@GetMapping("/Baby/{offset}/{size}/{field}")
+	public List<Baby> getByPage(@PathVariable int offset ,@PathVariable int size,@PathVariable String field){
+		return (service.getBabyByPagination(offset, size,field));
+	}
+	
 }
+
